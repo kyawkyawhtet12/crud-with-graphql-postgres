22b3555f03974fd9d7fa5eff981dd19223534121
Kyaw Kyaw Htet <kyawhtet7675@gmail.com>
2023-01-28 22:57:46 +0630

    crud

---
+++ database/factories/EmployeeFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Employee>
+ */
+class EmployeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $prefix = 'EE';
+        return [
+            'employee_id' => $prefix. "-".fake()->unique()->numberBetween(1, 99),
+            'full_name' => fake()->name,
+            'gender' => fake()->randomElement(['male', 'female', 'other']),
+            'age' => fake()->numberBetween(18, 60),
+            'hire_date' => fake()->date(),
+            'annual_salary' => fake()->numberBetween(400000,500000),
+            'bonus' => fake()->numberBetween(400000,500000)
+        ];
+    }
+}
---
+++ database/migrations/2023_01_28_141539_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('employee_id')->unique();
+            $table->string('full_name');
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->integer('age');
+            $table->date('hire_date');
+            $table->integer('annual_salary');
+            $table->integer('bonus');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
